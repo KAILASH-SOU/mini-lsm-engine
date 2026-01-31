f326a0d01c945122a4d6139769dc3462668f53ac
Kailash <kailash05072005@gmail.com>
2026-02-01 02:15:12 +0530

    Flush MemTable to SSTables and integrate disk reads

--- include/engine.h
+++ include/engine.h
@@ -3,16 +3,22 @@
 #include "wal.h"
 #include "memtable.h"
 #include <string>
+#include <vector>
+#include <optional>
 
 class Engine {
 public:
     explicit Engine(const std::string& wal_path);
 
-    // User API
     void put(const std::string& key, const std::string& value);
     std::optional<std::string> get(const std::string& key);
 
 private:
     WAL wal_;
     MemTable memtable_;
+
+    
+    std::vector<std::string> sstables_;
+
+    void flush_memtable();
 };
--- src/engine.cpp
+++ src/engine.cpp
@@ -1,4 +1,7 @@
 #include "engine.h"
+#include "sstable.h"
+#include <filesystem>
+
 
 Engine::Engine(const std::string& wal_path)
     : wal_(wal_path) {
@@ -6,13 +9,46 @@ Engine::Engine(const std::string& wal_path)
 }
 
 
-void Engine::put(const std::string& key, const std::string& value) {
-    
-    wal_.append(key, value);
 
+void Engine::put(const std::string& key, const std::string& value) {
+    wal_.append(key, value);
     memtable_.put(key, value);
+
+    const size_t MEMTABLE_FLUSH_LIMIT = 3;
+    if (memtable_.size() >= MEMTABLE_FLUSH_LIMIT) {
+        flush_memtable();
+    }
 }
 
+
 std::optional<std::string> Engine::get(const std::string& key) {
-    return memtable_.get(key);
+    
+    auto val = memtable_.get(key);
+    if (val) return val;
+
+    
+    for (auto it = sstables_.rbegin(); it != sstables_.rend(); ++it) {
+        std::string value;
+        if (SSTable::get(*it, key, value)) {
+            return value;
+        }
+    }
+
+    return std::nullopt;
+}
+
+
+void Engine::flush_memtable() {
+    if (memtable_.size() == 0) return;
+
+    std::filesystem::create_directories("data/sstables");
+
+    std::string filename =
+        "data/sstables/sstable_" + std::to_string(sstables_.size()) + ".dat";
+
+    SSTable::write(filename, memtable_.data());
+
+    sstables_.push_back(filename);
+
+    memtable_.clear();
 }
